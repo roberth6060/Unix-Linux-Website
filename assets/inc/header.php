a8492f1cd0de52fdf3fc3e1f419d2e753c3e08af
Robert C. Haye <roberthaye1994@gmail.com>
2023-04-05 02:44:54 +0200

    update content of navbar

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,17 +1,93 @@
-<!-- 
-Name: Robert Haye
-Date: March 26, 2023
- -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Order Form</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/pizza.css" />
+    <title>Unix/Linux</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css" />
     <script type="text/javascript" src="assets/script/pizza.js"></script>
 </head>
 
 <body>
-    <div id="wrapper">
-        <h1>Eat more Pizza!</h1>
-        <p><img src="assets/img/hotpizza.png"></p>
+    <nav>
+        <a href="../index.html" class="logo">
+            <img src="" alt="logo" />
+            <span>Travel Blog Website</span>
+        </a>
+        <input id="menu-toggle" type="checkbox" />
+        <label for="menu-toggle" class="menu-button-container">
+            <span class="menu-button"></span>
+        </label>
+        <ul class="menu">
+            <li><a href="../index.html">Home</a></li>
+            <li>
+                <a href="./about.html">About</a>
+            </li>
+            <li class="excercises">
+                <a href="#">Unix</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="./document.html">The Essential Guide </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="excercises">
+                <a href="#">Distributions</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="./document.html">Beginner-Friendly</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Distribution for Servers</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="excercises">
+                <a href="#">Security</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="./document.html">Linux Security Principles</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Security Requirements</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Enemy</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="excercises">
+                <a href="#">Installation</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="./document.html">Install Linux on Windows</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Bootable Linux USB</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Install App on Linux OS</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Ubuntu Software Center</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">APT Installation</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Synaptic Package Manager</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="excercises">
+                <a href="#">Resources</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="./document.html">References</a>
+                    </li>
+                    <li>
+                        <a href="./references.html">Download Links</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@ include $path."assets/inc/header.php";
 ?>
 
 <div id="container">
-<h2>Group project at RIT Croatia</h2>
+    <h2>Group project at RIT Croatia</h2>
+    <p>This project</p>
 </div>
 <?php
 include $path."assets/inc/footer.php";
