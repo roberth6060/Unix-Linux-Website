2594b34b44fb282ed667011adbeadb92e4b1c433
Robert C. Haye <roberthaye1994@gmail.com>
2023-04-19 02:08:14 +0200

    update main theme of website

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,4 +1,11 @@
-<footer>&copy; Copyright 2023, (Dys)functional</footer>
+<footer>
+    <div class="brdCrmbTrl">
+        <a href="#">Home</a> >
+        <a href="#">Somewhere</a> >
+        <a href="#">Here</a>
+    </div>
+    <div class="logo footLog">&copy; Copyright 2023, (Dys)functional</div>
+</footer>
 </div>
 </body>
 
